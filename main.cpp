f4132254fdff96090c5f5eeb7ff1682785d9c2f9
ALARNINJAD <alarninjad@gmail.com>
2025-01-19 11:00:46 +0330

    change vector structure and add show_map

--- main.cpp
+++ main.cpp
@@ -5,22 +5,43 @@ using namespace std;
 
 class Map {
 
-    vector <string> map;
+    vector <vector<char>> map;
 
 public:
 
     Map(string fileName) {
-        ifstream file(fileName);
-        if ( !file.is_open() ) { cerr << "File error" << endl; }
-        string str;
-        while (getline(file, str)) { map.push_back(str); }
+
+        ifstream file(fileName); // open file on reading mode
+        if ( !file.is_open() ) { cerr << "File error" << endl; } // error handling
+
+        // read text and write to 2d vector
+        string str;                                        // str is for each line of text
+        while ( getline(file, str) ){                      // reading each line of file and write it to str 
+            vector <char> row;                             // a temporarly vector for breaking str to chars
+            for ( int i=0 ; i<str.size() ; i++ )           // str to vector of chars
+                row.push_back(str[i]);
+            map.push_back(row);                            // push each row to the map
+            row.clear();                                   // making row fresh for each line
+        }
+
+        // close the file
         file.close();
+    }
+
+    void show_map (){
+        for ( int i=0 ; i<map.size() ; i++ ){           // column
+            for ( int j=0 ; j<map[i].size() ; j++ )     // row
+                cout << map[i][j];
+            cout << endl;
+        }
     }
 };
 
-int main() {
+int main (){
     
 	Map maze("map.txt");
+
+    maze.show_map();
 
     return 0;
 }
