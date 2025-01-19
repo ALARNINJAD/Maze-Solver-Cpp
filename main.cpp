7fdf2d4150aebf6497b41884ca9ce7839d40f5ad
ALARNINJAD <alarninjad@gmail.com>
2025-01-19 10:17:39 +0330

    add error handling for file

--- main.cpp
+++ main.cpp
@@ -4,22 +4,23 @@
 using namespace std;
 
 class Map {
+
+    vector <string> map;
+
 public:
-    vector<string> map;
 
     Map(string fileName) {
         ifstream file(fileName);
+        if ( !file.is_open() ) { cerr << "File error" << endl; }
         string str;
-        while (getline(file, str)) {
-            map.push_back(str);
-        }
+        while (getline(file, str)) { map.push_back(str); }
         file.close();
     }
 };
 
 int main() {
     
-	Map x("map.txt");
+	Map maze("map.txt");
 
     return 0;
 }
